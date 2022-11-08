999cccab2f061d16868b3d643140207166ac047b
Reza Ghahremani <developer.ghahremani@gmail.com>
2022-11-08 19:21:09 +0330

    add preffix

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
